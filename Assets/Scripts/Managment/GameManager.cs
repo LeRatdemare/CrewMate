f76df2d3bfce38eb7f9ba7db1f676c99784becd6
Nathan Lufuluabo <lufuluabon@outlook.fr>
2023-02-06 13:12:00 +0100

    Update : Création du Prefab pour les cartes

--- Assets/Scripts/Managment/GameManager.cs
+++ Assets/Scripts/Managment/GameManager.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject cardPrefab;
     // Start is called before the first frame update
     void Start()
     {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
+        for (int i = 0; i < 3; i++)
+        {
+            Vector3 pos = new Vector3(i * 20, 0, 0);
+            GameObject card = Instantiate(cardPrefab, pos, Quaternion.identity);
+            card.GetComponent<Image>().sprite = Resources.Load<Sprite>("suit_life_meter_2");
+        }
     }
 }
