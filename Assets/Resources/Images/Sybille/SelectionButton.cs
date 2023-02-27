87672737dda1b8586f0ba342bbb190493e0d26aa
Sybille Lafont <sylafont@ensc.fr>
2023-02-27 15:47:17 +0000

    Changement SelectionButton

--- Assets/Resources/Images/Sybille/SelectionButton.cs
+++ Assets/Resources/Images/Sybille/SelectionButton.cs
@@ -4,5 +4,7 @@ using UnityEngine;
 
 public class SelectionButton : MonoBehaviour
 {
-    public static int BouttonJoueur;
+    public static int BouttonJoueur; 
+    public static List<GameObject> boutons = new List<GameObject> (){GameObject.Find("ButtonPlayer3"),GameObject.Find("ButtonPlayer4"), GameObject.Find("ButtonPlayer5")};
+
 }
