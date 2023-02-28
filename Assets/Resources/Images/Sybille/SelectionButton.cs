f1e769ef9aed140e1e52afc4e5c94f9e2f3c8cbc
Sybille Lafont <sylafont@ensc.fr>
2023-02-28 13:26:27 +0000

    Bouton selectionnées marchent!!!

--- Assets/Resources/Images/Sybille/SelectionButton.cs
+++ Assets/Resources/Images/Sybille/SelectionButton.cs
@@ -5,12 +5,21 @@ using UnityEngine.UI;
 
 public class SelectionButton : MonoBehaviour
 {
-    //public static int bouttonJoueur;
+    //public static int bouttonJoueur =0;
     //public List<GameObject> boutons = new List<GameObject>{GameObject.Find("ButtonPlayer3"),GameObject.Find("ButtonPlayer4"),GameObject.Find("ButtonPlayer5")}; 
-
+    public static List<int> bouttonPrevious = new List<int>{0,0};
+    public Sprite sp1;
+    public Sprite sp2;
+    public string typeBoutton;
+    public int type;
+   
     public void ButtonSelected(int numero){
-        //bouttonJoueur = numero;
-        GameObject.Find("ButtonPlayer"+$"{numero}").GetComponent<Image>().sprite=Resources.Load<Sprite>("Images/Sybille/BoutonPressed");
+        if (bouttonPrevious[type] != numero && bouttonPrevious[type] !=0){
+            GameObject.Find(typeBoutton+$"{bouttonPrevious[type]}").GetComponent<Image>().overrideSprite=sp2;
+        }
+        GameObject.Find(typeBoutton+$"{numero}").GetComponent<Image>().overrideSprite=sp1;
+        bouttonPrevious[type] = numero;
+        //Resources.Load<Sprite>("BoutonPressed");
     }
         
         
