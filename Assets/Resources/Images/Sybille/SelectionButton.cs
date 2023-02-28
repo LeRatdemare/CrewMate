697ab7608526c5095c177fd6eb27500e7a777a8a
Sybille Lafont <sylafont@ensc.fr>
2023-02-28 10:49:05 +0000

    Tentative selection bouton

--- Assets/Resources/Images/Sybille/SelectionButton.cs
+++ Assets/Resources/Images/Sybille/SelectionButton.cs
@@ -1,10 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SelectionButton : MonoBehaviour
 {
-    public static int BouttonJoueur; 
-    public static List<GameObject> boutons = new List<GameObject> (){GameObject.Find("ButtonPlayer3"),GameObject.Find("ButtonPlayer4"), GameObject.Find("ButtonPlayer5")};
+    //public static int bouttonJoueur;
+    //public List<GameObject> boutons = new List<GameObject>{GameObject.Find("ButtonPlayer3"),GameObject.Find("ButtonPlayer4"),GameObject.Find("ButtonPlayer5")}; 
 
+    public void ButtonSelected(int numero){
+        //bouttonJoueur = numero;
+        GameObject.Find("ButtonPlayer"+$"{numero}").GetComponent<Image>().sprite=Resources.Load<Sprite>("Images/Sybille/BoutonPressed");
+    }
+        
+        
+    
 }
