15f3431f7abd7f923c3449c5472f4b7940d0142a
Sybille Lafont <sylafont@ensc.fr>
2023-03-28 07:35:43 +0100

    Des trucs

--- Assets/Resources/Images/Sybille/CheckInt.cs
+++ Assets/Resources/Images/Sybille/CheckInt.cs
@@ -5,9 +5,22 @@ using UnityEngine.UI;
 
 public class CheckInt : MonoBehaviour
 {
-    public void CheckValue(string name){
-        //int i;
-        GameObject.Find(name).GetComponent<Text>();  
-        //bool result = int.TryParse(s, out i);
+    public void CheckValeur(string name){
+        int i;
+        Debug.Log("Bonjour,");
+        Text s = GameObject.Find(name).GetComponent<Text>(); 
+        Debug.Log("Bonjour2"); 
+        Debug.Log("Bonjour3");
+        string input = s.text;
+        Debug.Log("Bonjour4");
+        bool result = int.TryParse(input, out i);
+        if (result == false){
+            for(int j =0; j<7;j++){
+                GameObject.Find("ErreurIntMiss").SetActive(true);// ca va pas le nom de l'objet
+                System.Threading.Thread.Sleep(500);
+                GameObject.Find("ErreurIntMiss").SetActive(false);
+            }
+
+        }
     }
 }
