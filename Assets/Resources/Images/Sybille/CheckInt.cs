9195d5e6dafde1c423d2fedc6f697d103a35e2a9
Sybille Lafont <sylafont@ensc.fr>
2023-03-31 10:59:33 +0100

    Les inputfield n'acceptent que les nombres entiers

--- Assets/Resources/Images/Sybille/CheckInt.cs
+++ Assets/Resources/Images/Sybille/CheckInt.cs
@@ -5,22 +5,59 @@ using UnityEngine.UI;
 
 public class CheckInt : MonoBehaviour
 {
-    public void CheckValeur(string name){
+    public GameObject erreur;
+    public InputField b;
+    public static List<int> MissionTentative = new List<int>(){0,0};
+    public void CheckValeur(){
         int i;
-        Debug.Log("Bonjour,");
-        Text s = GameObject.Find(name).GetComponent<Text>(); 
-        Debug.Log("Bonjour2"); 
-        Debug.Log("Bonjour3");
-        string input = s.text;
-        Debug.Log("Bonjour4");
-        bool result = int.TryParse(input, out i);
-        if (result == false){
-            for(int j =0; j<7;j++){
-                GameObject.Find("ErreurIntMiss").SetActive(true);// ca va pas le nom de l'objet
-                System.Threading.Thread.Sleep(500);
-                GameObject.Find("ErreurIntMiss").SetActive(false);
-            }
-
+        
+        //Debug.Log("You entered :" + message);
+        //InputField input = GameObject.Find("InputField2").GetComponent<InputField>();
+        //GameObject input = GameObject.Find("InputField2/Text Area");
+        //InputField input = GameObject.Find("InputField2/Text Area").GetComponent<InputField>();
+        //Debug.Log(GameObject.Find("InputField2/Text Area/Text"));
+        //Debug.Log("hé");
+        //Debug.Log(input.text);
+        //Text s = input.GetComponent<Text>();
+        //string input = b.text;
+        //Text s = input.textComponent;
+        //Debug.Log("Bonjour2.5");
+        //Debug.Log(s);
+        //Debug.Log("Bonjour2"); 
+        //Debug.Log("Bonjour3");
+        //string input = s.text;
+        //Debug.Log("Bonjour4");
+        bool result = int.TryParse(b.text, out i);
+        if (result == false || i>50){
+            ChangeValueListe(0);
+            b.Select();
+            b.text ="";
+            StartCoroutine(Wait());  
         }
+        else{
+            ChangeValueListe(i);
+        }
+        //Debug.Log($"Valeur Liste[0] : {MissionTentative[0]}, Valeur Liste[1] : {MissionTentative[1]}");
     }
+
+        IEnumerator Wait()
+        {
+            for (int i=0; i<4;i++){
+                erreur.SetActive(true);
+                yield return new WaitForSeconds(1);   
+                erreur.SetActive(false);
+                yield return new WaitForSeconds(1);  
+            }
+            
+        }
+
+        public void ChangeValueListe(int nombre){
+            if (b.name == "InputField1"){
+                MissionTentative[0]= nombre;
+            }
+            else{
+                MissionTentative[1]= nombre;
+            }
+        }
+
 }
