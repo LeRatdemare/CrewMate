88c798ecec2eecf082c77b794ffab20427d64d7a
Sybille Lafont <sylafont@ensc.fr>
2023-04-04 21:24:17 +0100

    Peaufinage Bouton pour valider

--- Assets/Resources/Images/Sybille/ButtonValidate.cs
+++ Assets/Resources/Images/Sybille/ButtonValidate.cs
@@ -5,7 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class ButtonValidate : MonoBehaviour
 {
+    //public SelectionButton selectionButton;
     public void Validate(){
-        SceneManager.LoadScene("SettingMenu");
+        bool complet = true;//Initialisation de la variable qui vérifie que le joueur a rentré toutes les infos
+        for (int i=0;i<2;i++){
+            if (SelectionButton.bouttonPrevious[i]==0){
+                complet =false;
+            }
+        }
+        if (complet==true){
+            SceneManager.LoadScene("SettingMenu");
+        }
     }
 }
--- Assets/Valider.cs
+++ Assets/Valider.cs
@@ -6,38 +6,34 @@ using UnityEngine.SceneManagement;
 public class Valider : MonoBehaviour
 {
     public static List<int> parametrePartie = new List<int>{0,0,0,0,0};
+    // il y a 5 valeurs 
     public CheckInt checkInt;
-    public SelectionButton selectionButton;
+    //public SelectionButton selectionButton;
     // Changer variable du jeton de bool vers int (0/1)
-    public bool ButtonValider(){
-        int s=0;
-        for (int i=0; i<3;i++){
-            if (SelectionButton.bouttonPrevious[i]!=0){
-                SelectionButton.bouttonPrevious[i] = parametrePartie[i]; 
-                s++;
-            }
+    public void ButtonValider(){
+        bool complet = true;
+        Debug.Log("here1");
+            if (SelectionButton.bouttonPrevious[2]!=0){
+                Debug.Log("here2");
+                SelectionButton.bouttonPrevious[2] = parametrePartie[2];
+                Debug.Log("here3");
+                for(int i=0; i<2;i++){
+                    if(CheckInt.MissionTentative[i] !=0){
+                        Debug.Log("here4");
+                        CheckInt.MissionTentative[i] = parametrePartie[3+i];
+                        Debug.Log("here5");
+                    }
+                    else{
+                        complet= false;
+                        break;
+                    }
+                }
+            }    
             else{
-                return false;
+                complet = false;
             }
+        if(complet==true){
+            SceneManager.LoadScene("Jeu");
         }
-        for(int i=0; i<2;i++){
-            if(CheckInt.MissionTentative[i] ==0){
-                CheckInt.MissionTentative[i] = parametrePartie[s+i];
-            }
-            else{
-                return false;
-            }   
-        }
-        SceneManager.LoadScene("");
-        return true;
-
-        
-
-        
-
-        
-        
-        //Vérif que soit 2 soit 5
-        //load Scene(nameScene)
     }
 }
