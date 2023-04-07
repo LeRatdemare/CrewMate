995c76cc7bde3615b6a7da1d4c5a1a560d452de6
Sybille Lafont <sylafont@ensc.fr>
2023-04-07 14:53:48 +0100

    Enlever com qui servait à rien

--- Assets/Resources/Images/Sybille/CheckInt.cs
+++ Assets/Resources/Images/Sybille/CheckInt.cs
@@ -10,23 +10,6 @@ public class CheckInt : MonoBehaviour
     public static List<int> MissionTentative = new List<int>(){0,0};
     public void CheckValeur(){
         int i;
-        
-        //Debug.Log("You entered :" + message);
-        //InputField input = GameObject.Find("InputField2").GetComponent<InputField>();
-        //GameObject input = GameObject.Find("InputField2/Text Area");
-        //InputField input = GameObject.Find("InputField2/Text Area").GetComponent<InputField>();
-        //Debug.Log(GameObject.Find("InputField2/Text Area/Text"));
-        //Debug.Log("hé");
-        //Debug.Log(input.text);
-        //Text s = input.GetComponent<Text>();
-        //string input = b.text;
-        //Text s = input.textComponent;
-        //Debug.Log("Bonjour2.5");
-        //Debug.Log(s);
-        //Debug.Log("Bonjour2"); 
-        //Debug.Log("Bonjour3");
-        //string input = s.text;
-        //Debug.Log("Bonjour4");
         bool result = int.TryParse(b.text, out i);
         if (result == false || i>50){
             ChangeValueListe(0);
@@ -59,5 +42,4 @@ public class CheckInt : MonoBehaviour
                 MissionTentative[1]= nombre;
             }
         }
-
 }
